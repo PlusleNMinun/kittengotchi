383076b93a60bf5b6ecd68b04e752599db362db5
PlusleNMinun <48526288+PlusleNMinun@users.noreply.github.com>
2019-10-31 23:45:19 -0400

    add files via upload

--- canvas.js
+++ canvas.js
@@ -1,16 +1,13 @@
-// Responsive Canvas to 80%
-window.onload = window.onresize = function() {
-  var canvas = document.getElementById('canvas');
-  canvas.width = window.innerWidth * 0.8;
-  canvas.height = window.innerHeight * 0.8;
-}
-
-var canvas = document.getElementById("myCanvas");
-
-// Drawing Element
-var ctx = canvas.getContext("2d");
-
-// Rectangle
-ctx.fillStyle = "#FF0000";
-
-ctx.fillRect(0, 0, 150, 75);
+// // Responsive Canvas to 80%
+// window.onload = window.onresize = function() {
+//   var canvas = document.getElementById('canvas');
+//   canvas.width = window.innerWidth * 0.8;
+//   canvas.height = window.innerHeight * 0.8;
+// }
+//
+// var canvas = document.getElementById("myCanvas");
+//
+// // Drawing Element
+// var ctx = canvas.getContext("2d");
+//
+// // Image
